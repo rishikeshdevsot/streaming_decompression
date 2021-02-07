d09c577398239ed7b88297d8fcefbeccb18951c8
Rishikesh Devsot <rishikeshdevsot@gmail.com>
2021-02-07 14:25:31 -0500

    Able to read the archive contents piped from cat

--- src/program_1.cpp
+++ src/program_1.cpp
@@ -2,6 +2,9 @@
 #include <archive_entry.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <cstring>
 
 // Resources
 // http://www.alecjacobson.com/weblog/?p=2075
@@ -10,10 +13,20 @@ int main(){
     struct archive_entry *entry;
     int r;
 
+    std::stringstream ss;
+    std::string archiveInputLine;
+
+    while(getline(std::cin, archiveInputLine)){ 
+        ss << archiveInputLine << "\n";
+    }
+    std::string fullArchive(ss.str());
+    char fullArchiveBuf[fullArchive.length() + 1];
+    
     a = archive_read_new();
     archive_read_support_filter_all(a);
     archive_read_support_format_all(a);
-    r = archive_read_open_filename(a, "../samples/archive.tar.gz", 10240); // Note 1
+    r = archive_read_open_memory(a, &fullArchive[0], fullArchive.length());
+    //r = archive_read_open_filename(a, "../samples/archive.tar.gz", 10240); // Note 1
     if (r != ARCHIVE_OK)
         exit(1);
     while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
