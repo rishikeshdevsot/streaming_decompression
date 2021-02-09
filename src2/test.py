f3ff189ad34afdb22eb84ebcb6015638a263a1d5
Rishikesh Devsot <rishikeshdevsot@gmail.com>
2021-02-09 00:32:58 -0500

    Working decompression and forwarding

---
+++ src2/test.py
@@ -0,0 +1,19 @@
+import tarfile
+import io
+import sys
+
+archive = '/home/rishi/Projects/sample_compression/samples/archive.tar'
+
+# Read input data.
+input_tar = tarfile.open(fileobj=sys.stdin, mode='r|*')
+output_tar = tarfile.open(fileobj=sys.stdout, mode='w|')
+tarinfo = input_tar.next()
+while(tarinfo != None):
+    fileobj = input_tar.extractfile(tarinfo)
+
+    # Write output file.
+    output_tar.addfile(tarinfo, fileobj)
+    tarinfo = input_tar.next()
+
+input_tar.close()
+output_tar.close()
