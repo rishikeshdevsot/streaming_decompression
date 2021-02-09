4843da160dabb8478d75dbb4e82c997b66a39ee3
Rishikesh Devsot <rishikeshdevsot@gmail.com>
2021-02-08 23:47:38 -0500

    Extraction from stdin

--- src2/program1.py
+++ src2/program1.py
@@ -1,2 +1,20 @@
 #Resources
 #https://docs.python.org/3/library/shutil.html#module-shutil
+# encoding = ISO-8859-1
+
+import gzip
+import sys
+import io
+import shutil
+
+import tarfile
+import sys
+
+tar = tarfile.open(fileobj=sys.stdin, mode='r|*')
+tar.extractall()
+tar.close()
+
+
+
+#with gzip.open(sys.stdin, 'rb') as f:
+#    file_content = gzip.decompress(f.read())
