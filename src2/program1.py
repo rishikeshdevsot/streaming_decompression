b62cc690fe0a7b734a0ee52c978aaba0a55dd9a8
Rishikesh Devsot <rishikeshdevsot@gmail.com>
2021-02-09 17:02:23 -0500

    Made both python files executable and filtered symlink extraction

--- src2/program1.py
+++ src2/program1.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+
+# tarfile doesn't support symlink extraction https://duplicity.readthedocs.io/en/latest/_modules/tarfile.html ("cannot extract (sym)link as file object")
+# https://bugs.python.org/issue35483
+# https://stackoverflow.com/questions/10060069/safely-extract-zip-or-tar-using-python
+# (cannot use support symlinks)
+
 import tarfile
 import io
 import sys
@@ -6,13 +13,15 @@ import sys
 # Read input data.
 input_tar = tarfile.open(fileobj=sys.stdin, mode='r|*')
 output_tar = tarfile.open(fileobj=sys.stdout, mode='w|')
+
 tarinfo = input_tar.next()
 while(tarinfo != None):
-    fileobj = input_tar.extractfile(tarinfo)
-
-    # Write output file.
-    output_tar.addfile(tarinfo, fileobj)
+    if(not tarinfo.issym()):
+        fileobj = input_tar.extractfile(tarinfo)
+        # Write output file.
+        output_tar.addfile(tarinfo, fileobj)
     tarinfo = input_tar.next()
 
+sys.stdout.flush()
 input_tar.close()
 output_tar.close()
--- src2/program2.py
+++ src2/program2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import tarfile
 import io
 import sys
